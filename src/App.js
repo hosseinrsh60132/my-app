9cecef92d73be3696857a92dd113d6c425ff6f52
hossein rsh <hossein.rsh6013@gmail.com>
2023-06-05 14:07:13 +0430

    AddBootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Container } from 'react-bootstrap';
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
 
 const App = () => {
   return (
     <div>
-       hi there!
+        <Header />
+        <Container>
+            <h2>My Market</h2>
+        </Container>
+        <Footer />
     </div>
   )
 }
